4021ae89cbf6243ccb528e34edd95426919d511d
MarkPreedy <mark_preedy@hotmail.com>
2025-03-11 16:58:35 +0000

    start todo app

--- got_things2do/settings.py
+++ got_things2do/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'todo', # Add the todo app to the list of installed apps
 ]
 
 MIDDLEWARE = [
